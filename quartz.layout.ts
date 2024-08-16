11c0c6985175d719d114987ac618f6927baa6e08
isaac <vazquez.isaac98@gmail.com>
2024-08-15 19:41:05 -0500

    change graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,7 +28,34 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 14.1, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
+        fontSize: 0.8, // what size should the node labels be?
+        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+      globalGraph: {
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 3.9,
+        repelForce: 0.5,
+        centerForce: 0.3,
+        linkDistance: 30,
+        fontSize: 0.9,
+        opacityScale: 1,
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
