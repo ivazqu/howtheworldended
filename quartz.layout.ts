7e840a1c38bfeee7c036df49f14657433446e188
isaac <vazquez.isaac98@gmail.com>
2024-08-16 18:05:17 -0500

    graph changes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
         linkDistance: 30, // how long should the links be by default?
         fontSize: 0.5, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
-        removeTags: [], // what tags to remove from the graph
+        removeTags: true, // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
       },
       globalGraph: {
@@ -52,7 +52,7 @@ export const defaultContentPageLayout: PageLayout = {
         linkDistance: 30,
         fontSize: 0.5,
         opacityScale: 1,
-        removeTags: [], // what tags to remove from the graph
+        removeTags: true, // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
       },
     }),
