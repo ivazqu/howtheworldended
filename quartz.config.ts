8f4fb918ba0c9eec431ea6a17007cbaf0f3c7bc4
isaac <vazquez.isaac98@gmail.com>
2024-08-17 13:54:52 -0500

    revert

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "howtheworldended.com",
-    ignorePatterns: ["private", "templates"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
