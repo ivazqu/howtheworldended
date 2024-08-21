f03ac0484f9f52e0d328b327e27c730e55b7f99b
isaac <vazquez.isaac98@gmail.com>
2024-08-21 12:27:24 -0500

    trying to fix tags

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -43,6 +43,7 @@ async function renderGraph(container: string, fullSlug: FullSlug) {
     fontSize,
     opacityScale,
     removeTags,
+    showTags,
     focusOnHover,
   } = JSON.parse(graph.dataset["cfg"]!)
 
